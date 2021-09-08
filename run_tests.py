aa3ea28e02f74a62ac6196922a7fccae46a68716
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2021-09-08 18:34:53 -0500

    adding more test to give aiomultiprocess the benefit of the doubt

--- run_tests.py
+++ run_tests.py
@@ -9,17 +9,23 @@ from functools import partial
 TITLE_REGEXP = re.compile(r'<title>(.*)</title>')
 IDXS = range(20)
 URL = 'https://google.com/'
+URLS = [URL] * 20
 
 
-async def get(url, idx):
+async def get(url):
+    async with request('GET', url) as response:
+        return await response.text('latin1')
+
+
+async def get_with_index(url, idx):
     async with request('GET', url) as response:
         return (await response.text('latin1'), idx)
 
 
-PARTIAL_GET = partial(get, URL)
+PARTIAL_GET = partial(get_with_index, URL)
 
 
-async def aiomultiprocess_test():
+async def aiomultiprocess_test_with_work():
     print('''
 ===========================aiomultiprocess======================================
 ''')
@@ -32,7 +38,7 @@ async def aiomultiprocess_test():
 ''')
 
 
-async def asyncio_test():
+async def asyncio_test_with_work():
     print('''
 ===========================asyncio==============================================
 ''')
@@ -46,14 +52,50 @@ async def asyncio_test():
 ''')
 
 
+async def aiomultiprocess_test_just_requests():
+    print('''
+===========================aiomultiprocess======================================
+''')
+    async with Pool() as pool:
+        # Just execute the requests
+        await pool.map(get, URLS)
+    print('done!')
+    print('''
+================================================================================
+''')
+
+
+async def asyncio_test_just_requests():
+    print('''
+===========================asyncio==============================================
+''')
+    tasks = [asyncio.create_task(get(url)) for url in URLS]
+    await asyncio.gather(*tasks)
+    print('done!')
+    print('''
+================================================================================
+''')
+
+
+
 if __name__ == '__main__':
-    start_multi = time.time()
-    asyncio.run(aiomultiprocess_test())
-    end_multi = time.time()
+    start_multi_work = time.time()
+    asyncio.run(aiomultiprocess_test_with_work())
+    end_multi_work = time.time()
 
-    start_asyncio = time.time()
-    asyncio.run(asyncio_test())
-    end_asyncio = time.time()
+    start_asyncio_work = time.time()
+    asyncio.run(asyncio_test_with_work())
+    end_asyncio_work = time.time()
 
-    print(f'aiomultiprocess: {end_multi - start_multi}s')
-    print(f'asyncio: {end_asyncio - start_asyncio}s')
+    start_multi_requests = time.time()
+    asyncio.run(aiomultiprocess_test_just_requests())
+    end_multi_requests = time.time()
+
+    start_asyncio_requests = time.time()
+    asyncio.run(asyncio_test_just_requests())
+    end_asyncio_requests = time.time()
+
+    print(f'aiomultiprocess - doing work: {end_multi_work - start_multi_work}s')
+    print(f'asyncio - doing work: {end_asyncio_work - start_asyncio_work}s')
+    print(f'aiomultiprocess - just requests: {end_multi_requests - start_multi_requests}s')
+    print(f'asyncio - just requests: {end_asyncio_requests - start_asyncio_requests}s')
