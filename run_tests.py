2da6479e14fbc9ef12e024e8e0e78c06aab35661
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2021-09-08 19:30:42 -0500

    not even with cpu-bound work!

--- run_tests.py
+++ run_tests.py
@@ -10,6 +10,7 @@ TITLE_REGEXP = re.compile(r'<title>(.*)</title>')
 IDXS = range(20)
 URL = 'http://chauvel.org'
 URLS = [URL] * 200
+TO_BE_FIBONACCIED = range(1000)
 
 
 async def get(url):
@@ -20,6 +21,19 @@ async def get(url):
 async def get_with_index(url, idx):
     async with request('GET', url) as response:
         return (await response.text('latin1'), idx)
+
+
+async def fibonacci(n):
+    a = 0
+    b = 1
+    sum = 0
+    count = 1
+    while count <= n:
+        count += 1
+        a = b
+        b = sum
+        sum = a + b
+    return sum
 
 
 PARTIAL_GET = partial(get_with_index, URL)
@@ -77,6 +91,30 @@ async def asyncio_test_just_requests():
 ''')
 
 
+async def aiomultiprocess_test_cpu_bound_work():
+    print('''
+===========================aiomultiprocess======================================
+''')
+    async with Pool() as pool:
+        # Just execute the requests
+        await pool.map(fibonacci, TO_BE_FIBONACCIED)
+    print('done!')
+    print('''
+================================================================================
+''')
+
+
+async def asyncio_test_cpu_bound_work():
+    print('''
+===========================asyncio==============================================
+''')
+    tasks = [asyncio.create_task(fibonacci(n)) for n in TO_BE_FIBONACCIED]
+    await asyncio.gather(*tasks)
+    print('done!')
+    print('''
+================================================================================
+''')
+
 
 if __name__ == '__main__':
     start_multi_work = time.time()
@@ -95,7 +133,17 @@ if __name__ == '__main__':
     asyncio.run(asyncio_test_just_requests())
     end_asyncio_requests = time.time()
 
+    start_multi_fibonacci = time.time()
+    asyncio.run(aiomultiprocess_test_cpu_bound_work())
+    end_multi_fibonacci = time.time()
+
+    start_asyncio_fibonacci = time.time()
+    asyncio.run(asyncio_test_cpu_bound_work())
+    end_asyncio_fibonacci = time.time()
+
     print(f'aiomultiprocess - doing work: {end_multi_work - start_multi_work}s')
     print(f'asyncio - doing work: {end_asyncio_work - start_asyncio_work}s')
     print(f'aiomultiprocess - just requests: {end_multi_requests - start_multi_requests}s')
     print(f'asyncio - just requests: {end_asyncio_requests - start_asyncio_requests}s')
+    print(f'aiomultiprocess - cpu-bound work: {end_multi_fibonacci - start_multi_fibonacci}s')
+    print(f'asyncio - cpu-bound work: {end_asyncio_fibonacci - start_asyncio_fibonacci}s')
